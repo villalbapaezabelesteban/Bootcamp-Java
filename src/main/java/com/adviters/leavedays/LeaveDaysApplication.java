85a63085e02a540e14006011dbd00fa33631f15d
Abel Villalba <abel.villalba.ext@ank.app>
2023-02-02 10:56:30 -0300

    Basic Java Web Project with SpringBoot

---
+++ src/main/java/com/adviters/leavedays/LeaveDaysApplication.java
@@ -0,0 +1,13 @@
+package com.adviters.leavedays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveDaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveDaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adviters/leavedays/LeaveDaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adviters.leavedays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveDaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
